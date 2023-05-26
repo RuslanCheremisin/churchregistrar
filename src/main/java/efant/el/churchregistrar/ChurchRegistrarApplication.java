2bbb0b155abc422fb7d10bd3d2d09312e10e2acb
rus <ruslan.cheremisin@gmail.com>
2023-05-26 13:16:26 +1000

    init commit

---
+++ src/main/java/efant/el/churchregistrar/ChurchRegistrarApplication.java
@@ -0,0 +1,13 @@
+package efant.el.churchregistrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchRegistrarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChurchRegistrarApplication.class, args);
+    }
+
+}
